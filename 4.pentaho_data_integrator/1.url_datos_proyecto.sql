901683f212b25c95c2ea05be5bed7ef3a7658818
Stefany Mojica <stefymojica40@gmail.com>
2021-09-02 20:59:54 +0200

    Añadiendo URL de los CSV del proyecto The World Bank

---
+++ 4.pentaho_data_integrator/1.url_datos_proyecto.sql
@@ -0,0 +1,3 @@
+-- URL ARCHIVO COUNTRY: https://masterdatascience.s3.us-east-2.amazonaws.com/4.Inteligencia_negocio_visualizacion/WDICountry.csv
+-- URL ARCHIVO DATA: https://masterdatascience.s3.us-east-2.amazonaws.com/4.Inteligencia_negocio_visualizacion/WDIData.csv
+-- URL ARCHIVO SERIES: https://masterdatascience.s3.us-east-2.amazonaws.com/4.Inteligencia_negocio_visualizacion/WDISeries.csv
