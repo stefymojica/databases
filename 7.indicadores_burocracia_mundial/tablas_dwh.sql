1301e669fd12e84a9fbc45f0cc748d6ef7438948
Stefany Mojica <stefymojica40@gmail.com>
2021-09-27 18:09:34 +0200

    Creando script para generacion de tablas de STG y DWH

---
+++ 7.indicadores_burocracia_mundial/tablas_dwh.sql
@@ -0,0 +1,18 @@
+CREATE TABLE [dbo].[DIM_COUNTRY](
+	[ID_PAIS] [varchar](3) NOT NULL PRIMARY KEY,
+	[DESC_PAIS] [varchar](250) NULL,
+	[ISO2] [varchar](250) NULL,
+	[DESC_MONEDA] [varchar](250) NULL,
+	[DESC_REGION] [varchar](250) NULL,
+	[DESC_GRUPO] [varchar](250) NULL,
+	[System of National Accounts] [varchar](250) NULL,
+	[System of trade] [varchar](250) NULL
+) ON [PRIMARY]
+GO
+
+CREATE TABLE [dbo].[FACT_WWBI](
+	[ID_PAIS] [varchar](3) NULL,
+	[ID_AÑO] INT NULL,
+	[IN_VALUE] FLOAT NULL
+) ON [PRIMARY]
+GO
